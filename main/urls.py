5a7d5df324b86102578450e4a7795c246e3df7a3
louislautz <louislautz@gmx.net>
2021-12-10 13:10:01 +0200

    Added URL routing to games and boardgamers pages and added respective view functions

--- main/urls.py
+++ main/urls.py
@@ -8,4 +8,12 @@ app_name = 'main'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    # Page that shows all games 
+    path('games/', views.games, name='games'),
+    # Page that shows a specific game
+    path('games/<int:game_id>', views.game, name='game'),
+    # Page that shows all boardgamers
+    path('boardgamers/', views.boardgamers, name='boardgamers'),
+    # Page that shows a specific game
+    path('boardgamers/<int:boardgamer_id>', views.boardgamer, name='boardgamer'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
 
+from .models import Boardgamer, Game
+
 def index(request):
     """Home page for the BoardgameForum."""
     return render(request, 'main/index.html')
+
+def boardgamers(request):
+    """Shows all boardgamers"""
+    boardgamers = Boardgamer.objects.order_by('date_joined')
+    context = {'boardgamers': boardgamers}
+    return render(request, 'main/boardgamers.html', context)
+
+def boardgamer(request, boardgamer_id):
+    """Shows a specific boardgamer"""
+    boardgamer = Boardgamer.objects.get(id = boardgamer_id)
+    games = boardgamer.game_set.order_by('-date_added')
+    context = {'boardgamer': boardgamer, 'games': games}
+    return render(request, 'main/boardgamer.html', context)
+
+def games(request):
+    """Shows all games"""
+    games = Game.objects.order_by('date_added')
+    context = {'games': games}
+    return render(request, 'main/games.html', context)
+
+def game(request, game_id):
+    """Shows a specific game"""
+    game = Game.objects.get(id = game_id)
+    context = {'game': game}
+    return render(request, 'main/game.html', context)
