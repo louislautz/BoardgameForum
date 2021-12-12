fc357cf06fb9ec5cf8f77e3a14a4e51f59409486
louislautz <louislautz@gmx.net>
2021-12-12 22:36:28 +0200

    Added User Accounts and changed the built-in User model

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-from .models import Boardgamer
+#from .models import Boardgamer
 from .models import Game
 
+Boardgamer = get_user_model()
+
+#admin.site.register(User)
 admin.site.register(Boardgamer)
 admin.site.register(Game)
